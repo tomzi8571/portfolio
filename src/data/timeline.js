df3c5c476c695c2cc05c6f38fe584206a8dd7676
Thomas Zillinger <Thomas.Zillinger@gmx.at>
2025-09-16 18:48:56 +0200

    -updated -added certification

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,48 +1,59 @@
 export const timeline = [
-  {
-    period: '2016 – 2025',
-    title: 'Senior SAP Commerce Full Stack and Backend Engineer',
-    company: 'Netconomy GmbH',
-    description: 'Developed a 360° e‑commerce platforms using the enterprise <strong>SAP Commerce (Hybris)</strong> application framework.' +
-            'SAP Commerce is an enterprise e‑commerce platform and application framework for building and scaling omnichannel storefronts,' +
-            'providing product and content management, pricing and promotions, cart/checkout and order management, personalization, and robust integration APIs.' +
+    {
+        period: '2016 – 2025',
+        title: 'Senior SAP Commerce Full Stack and Backend Engineer',
+        company: 'Netconomy GmbH',
+        description: 'Developed a 360° e‑commerce platforms using the enterprise <strong>SAP Commerce Cloud</strong> (Hybris) application framework. ' +
+            'SAP Commerce is an enterprise e‑commerce platform and application framework for building and scaling omnichannel storefronts, ' +
+            'providing BSC and B2B solutions for product and content management, pricing and promotions, cart/checkout and order management, personalization, and robust integration APIs. ' +
             'Over the time I got to know and worked, developed and customized in all those areas in the Backend, Backoffice and Frontend. ' +
             'I worked as part of many multinational expert teams and in close contact with the customers to provide a ' +
-            'complete integrated solution in the customer\'s enterprise architecture and <strong>User-, Product-, Order- and Delivery Managament</strong>.' +
-            'I was involved in almost all areas of development, from conception to delivery. I gained a basic expertise in <strong>Frontend development</strong> ' +
+            'complete integrated solution in the customer\'s enterprise architecture and <strong>User-, Product-, Order- and Delivery Managament</strong>. ' +
+            'I was involved in almost all areas of development, from conception to delivery. ' +
+            'I specialized and have detailed knowledge as a <strong>Backend Developer</strong>, designing software architecture ' +
+            'and integrating new customer requirements into a legacy environment. For this I enjoyed having frequent knowledge exchanges with the stakeholders to ' +
+            'understand the legacy enterprise architecture and henceforth <strong>make informed software architectural decisions</strong>. This included getting to know internal and external ' +
+            'data, service and frontend providers, like PIM, Amplience, ESB and Marketplace solutions using various interfaces, like <strong>REST</strong>, ActiveMQ and others. ' +
+            'I gained a basic expertise in <strong>Frontend development</strong> ' +
             'with <strong>React</strong>, Redux, Vite and GraphQl. For this I worked in close cooperation with frontend professionals and with UX/UI designers. ' +
-            'I specialized and have detailed knowledge as a <strong>Backend Developer</strong> to design software architecture ' +
-            'and integrate new customer requirements into a legacy environment. For this I enjoyed having frequent knowledge exchanges with the stakeholders to '+
-            'understand the legacy enterprise architecture and henceforth <strong>make informed software architectural decisions</strong>. This included getting to know internal and external' +
-            'data, service and frontend providers, like PIM, Amplience, ESB, Mirakl which use various interfaces, like <strong>REST</strong>, ActiveMQ and others.' +
-            'I also worked on the DevOps side, creating and maintaining <strong>CI/CD</strong> pipelines using Jenkins, Docker and Kubernetes and leading frequent Production Deployments and' +
-            'ensuring code qualtiy with Testdriven Developemtn (<strong>TDD</strong>) and unit (<strong>JUnit</strong>, <strong>Mockito</strong>) and end-to-end integation testing.' +
-            'I also worked in areas of performance optimization and <strong>security</strong> hardening and handling high traffic and securing against common <strong>vulnerabilities</strong>.' +
+            'I also worked on the DevOps side, creating and maintaining <strong>CI/CD</strong> pipelines using Jenkins, Docker and Kubernetes and leading frequent Production Deployments and ' +
+            'ensuring code qualtiy with Testdriven Developemtn (<strong>TDD</strong>) and unit (<strong>JUnit</strong>, <strong>Mockito</strong>) and end-to-end integation testing. ' +
+            'I also worked in areas of performance optimization and <strong>security</strong> hardening and handling high traffic and securing against common <strong>vulnerabilities</strong>. ' +
             'I worked with multinational companies as clients with multi-billion revenues and millions of customers, such as <strong>XXXLutz</strong>, ' +
-            '<strong>Stäubli</strong>, <strong>Pluradent</strong>, <strong>Blue Tomato</strong>, <strong>Telekom Austria</strong>, and other companies.',
-    skills: [
-      'Spring Boot', 'React', 'CI/CD', 'Microservices', 'REST', 'Jenkins', 'Docker', 'Kubernetes', 'Agile/Scrum',
-      'JUnit', 'Mockito', 'SAP Commerce (Hybris)', 'Mirakl', 'ActiveMQ', 'ESB', 'SQL', 'Caching (Varnish, Nginx)',
-      'Security', 'XSS', 'CSRF', 'SQL Injection', 'Jira', 'Confluence', 'GitHub',
-    ],
-  },
-  {
-    period: '2001 – 2016',
-    title: 'Full‑stack Developer',
-    company: 'Infonova GmbH / Beyond Now',
-    description: '<strong>Full‑stack engineer</strong> developing a multi‑tenant <strong>servicing and provisioning</strong> platform for large multinational'+
-                'telecom enterprises such as <strong>Telekom Austria</strong>.',
-    skills: ['Java', 'Hibernate', 'Oracle', 'SQL', 'jBPM', 'JSP/Servlet/JSTL', 'JUnit', 'Jenkins', 'Scrum'],
-  },
-  {
-    period: '',
-    title: 'B.Sc. Computer Science and Telematics',
-    company: 'Technical University, Graz, Austria',
-    description: 'Bachelor\'s <strong>thesis</strong>: “A General Web‑Interface of an Assessment Engine Instantiated for a TP‑based Mathematics Assistant.”',
-    skills: [
-      'Software Engineering', 'Software Systems & Algorithms', 'Neuroinformatics', 'Medical Technology',
-      'Advanced Math & Physics', 'Electrical Engineering',
-    ],
-  }
-]
+            '<strong>Hofer (Aldi)</strong>, <strong>Stäubli</strong>, <strong>Pluradent</strong>, <strong>Blue Tomato</strong>, <strong>Telekom Austria</strong>, and other companies.',
+        skills: [
+            'SAP Commerce Cloud (Hybris)', 'Java', 'Spring DI', 'CI/CD', 'Jenkins', 'Docker', 'Kubernetes', 'Microservices', 'REST', 'Agile/Scrum',
+            'TDD', 'JUnit', 'Mockito', 'Mirakl', 'ActiveMQ', 'React', 'ESB', 'SQL', 'Caching (Varnish, Nginx)',
+            'Security', 'XSS', 'CSRF', 'SQL Injection', 'Jira', 'Confluence', 'GitHub', 'Design Patterns'
+        ],
+    },
 
+    {
+        period: '2017',
+        title: 'SAP Certified Development Professional',
+        company: 'Certification',
+        url: 'https://www.credly.com/badges/7dfadb6d-c748-4d11-9b08-181d338c0ca9',
+        description: 'SAP Commerce Cloud 6.7 Developer',
+        skills: ['SAP Commerce Cloud', 'Java', 'Spring DI', 'Caching Strategies', 'OCC REST API', 'Security', 'Service Layer Architecture',
+            'Unit/Integration Testing',
+            'Flexible Search', 'Impex', 'Solr', 'Performance Optimization', 'Data Hub', 'SAP Backoffice Customization'],
+    },
+    {
+        period: '2001 – 2016',
+        title: 'Full‑stack Developer',
+        company: 'Bearingpoint Infonova GmbH / Beyond Now',
+        description: '<strong>Full‑stack engineer</strong> developing a multi‑tenant <strong>servicing and provisioning</strong> platform for large multinational' +
+            'telecom enterprises such as <strong>Telekom Austria</strong>.',
+        skills: ['Java', 'Hibernate', 'Oracle', 'SQL', 'jBPM', 'JSP/Servlet/JSTL', 'JUnit', 'Jenkins', 'Scrum'],
+    },
+    {
+        period: '',
+        title: 'B.Sc. Computer Science and Telematics',
+        company: 'Technical University, Graz, Austria',
+        description: 'Bachelor\'s <strong>thesis</strong>: “A General Web‑Interface of an Assessment Engine Instantiated for a TP‑based Mathematics Assistant.”',
+        skills: [
+            'Software Engineering', 'Software Systems & Algorithms', 'Neuroinformatics', 'Medical Technology',
+            'Advanced Math & Physics', 'Electrical Engineering',
+        ],
+    }
+]
